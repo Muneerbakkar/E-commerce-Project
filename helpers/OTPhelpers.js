09a21257d997a8f66904b90c1946b694db455275
Muneerbakkar <112262924+Muneerbakkar@users.noreply.github.com>
2023-01-01 22:28:06 +0530

    Delete OTPhelpers.js

--- helpers/OTPhelpers.js
+++
@@ -1,5 +0,0 @@
-// module.exports={
-//   serviceId:"VAc6128bffa5d3bfef50ff766d4muneer",
-//   accoundSid:"ACee275b46f270d394973b53208d21cmuneer",
-//   authToken:"72f1cdf1bdb1b85f6ed3a0ae17e8damuneer"
-// }
